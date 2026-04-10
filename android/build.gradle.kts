cfd53746d64765241bb418548ee3644221f9f09b
wizard <victorfolorunsoofficial@gmail.com>
2026-04-10 10:44:19 +0100

    Section 2.2: add google-services Gradle plugin to root and app build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,12 @@ allprojects {
     }
 }
 
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
